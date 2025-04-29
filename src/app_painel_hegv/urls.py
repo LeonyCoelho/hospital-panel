cc8bef92f7003cf03b8ec9c2e92018e9a19f5d41
LeonyCoelho <leony.coelho@outlook.com>
2025-04-29 00:48:42 -0300

    update

--- src/app_painel_hegv/urls.py
+++ src/app_painel_hegv/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('painel/', views.painel, name='painel'),            # sem sala
+    path('painel/<str:sala_nome>/', views.painel, name='painel_sala'),  # com sala
     path('leitos', views.leitos, name='leitos'),
-    path('get_all_leitos', views.get_all_leitos, name='get_all_leitos'),
+    path('get_all_leitos/<str:sala_nome>/', views.get_all_leitos, name='get_all_leitos'),
 ]
--- src/app_painel_hegv/views.py
+++ src/app_painel_hegv/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Leito
+from django.views.decorators.http import require_GET
 
-def home(request):
-    return render(request,"home.html")
+def painel(request, sala_nome=None):
+    context = {
+        'sala_nome': sala_nome,  # Vai mandar o nome da sala (ou None)
+    }
+    return render(request, "home.html", context)
 
 def leitos(request):
     return render(request,"leitos.html")
 
-def get_all_leitos(request):
+@require_GET
+def get_all_leitos(request, sala_nome):
+    # Converte o nome recebido pra caixa alta (pra evitar erro de digitação)
+    sala_nome = sala_nome.upper()
+
+    # Filtrar leitos que pertencem à sala informada (comparando o nome de exibição)
     leitos = Leito.objects.all()
+    leitos_filtrados = [leito for leito in leitos if leito.get_sala_display().upper() == sala_nome]
+
     leitos_list = [{
         "id": leito.id,
         "numero": leito.numero,
@@ -19,5 +30,6 @@ def get_all_leitos(request):
         "alta": leito.alta,
         "sala": leito.get_sala_display(),
         "procedimento": leito.procedimento,
-    } for leito in leitos]
+    } for leito in leitos_filtrados]
+
     return JsonResponse({'leitos': leitos_list})
